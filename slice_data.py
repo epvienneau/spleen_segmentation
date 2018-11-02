de62e7ca5875807e64a6fa80b58aa3098bbb39f3
epvienneau <emelina@vienneau.io>
2018-11-02 17:26:48 -0500

    finished script to generate 2D slices of every volume in training set

--- slice_data.py
+++ slice_data.py
@@ -5,17 +5,22 @@ import nibabel as nib
 def main():
     img_folder = 'data/training/img/'
     label_folder = 'data/training/label/'
-    cat_folder = 'data/training/cat'
-
+    img_slices_folder = 'data/training/slices/img/'
+    label_slices_folder = 'data/training/slices/label/'
+    
     for filename in os.listdir(img_folder):
         img = nib.load(img_folder+filename).get_fdata()
         img = crop(img)
-    save(concat_img, cat_folder+'img_concatenated.nii.gz')
+        img_id = filename[0:-7]
+        for s in range(np.shape(img)[2]):
+            save(img[:,:,s], img_slices_folder+img_id+'_'+str(s)+'.nii.gz')
 
     for filename in os.listdir(label_folder):
         label = nib.load(label_folder+filename).get_fdata()
         label = crop(label)
-    save(concat_label, cat_folder+'label_concatenated.nii.gz')
+        label_id = filename[0:-7]
+        for s in range(np.shape(label)[2]):
+            save(label[:,:,s], label_slices_folder+label_id+'_'+str(s)+'.nii.gz')
 
 def crop(data):
     data = data[0:511:4, 0:511:4, :]
@@ -23,7 +28,7 @@ def crop(data):
 
 def save(data, name):
     img = nib.Nifti1Image(data, np.eye(4)) #provide identity matrix as affine transformation
-    img.to_filename(name) 
+    nib.save(img, name) 
 
 if __name__ == '__main__':
     main()
